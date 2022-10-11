8c824d480120d9447a0e6065e1d30f089fb34a3d
Geoff Thomas <geoffthomas@microsoft.com>
2022-10-11 18:01:32 +0100

    Start attempt at solving day 2 part 1

--- src/day02.rs
+++ src/day02.rs
@@ -1,5 +1,15 @@
 pub fn day02(input_lines: &str) -> (String, String) {
     let _ = input_lines;
+
+    for lines in input_lines.lines() {
+        let vec: Vec<&str> = lines.split(":").collect();
+
+        let password = vec[1];
+        let min: i32 = vec[0].split(" ").collect::<Vec<&str>>()[0].split("-").collect::<Vec<&str>>()[0].parse().expect("Parse Error");
+        let max: i32 = vec[0].split(" ").collect::<Vec<&str>>()[0].split("-").collect::<Vec<&str>>()[1].parse().expect("Parse Error");
+        let required_char: &str = vec[0].split(" ").collect::<Vec<&str>>()[1];
+    }
+
     let answer1 = 0;
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
